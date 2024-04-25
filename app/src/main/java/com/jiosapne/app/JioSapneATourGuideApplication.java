aee8fe4f76f83ec9adaba54783dbf31912f04e9d
Vinita Yadav <viniprabhuyadav@gmail.com>
2024-04-25 21:57:02 +0530

    Intial Commit for project

---
+++ app/src/main/java/com/jiosapne/app/JioSapneATourGuideApplication.java
@@ -0,0 +1,13 @@
+package com.jiosapne.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JioSapneATourGuideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JioSapneATourGuideApplication.class, args);
+	}
+
+}
---
+++ app/src/test/java/com/jiosapne/app/JioSapneATourGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiosapne.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JioSapneATourGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
